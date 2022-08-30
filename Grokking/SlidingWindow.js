57591d1179be902de5069aeda4118fb55b51034d
Erika Collins <erikasusancollins@gmail.comclear>
2022-08-30 09:56:29 -0600

    solo stufying

---
+++ Grokking/SlidingWindow.js
@@ -0,0 +1,102 @@
+// Introduction Problem
+
+// const AverageOfSubarrayOfSizeK = (array, k) => {
+//   const result = [];
+//   let sum = 0.0;
+//   let start = 0;
+
+//   for (let end = 0; end < array.length; end++){
+//     // Keep track of sum until we reach 3 elements in width
+//     sum += array[end];
+    
+//     // Once we have the sum of three elements
+//     if (end >= k - 1){
+//       // Push current average
+//       result.push(sum / k);
+      
+//       // Subtract first element from sum
+//       sum -= array[start];
+      
+//       // Increment start (sliding window at beginning)
+//       start += 1;
+//     }
+//   }
+//   return result;
+// }
+
+// console.log(AverageOfSubarrayOfSizeK([2, 1, 5, 1, 3, 2], 3)) // [2.6, 2.3, 3, 2]
+
+
+
+// Maximum Sum Subarray of Size K
+  // Time Complexity: 0(n)
+  // Space Complexity: 0(1)
+
+// const MaxSum = (array, k) => {
+//   let maxSum;
+//   let sum = 0.0;
+//   let start = 0;
+
+//   for (let end = 0; end < array.length; end++){
+//     // Keep track of sum until we reach 3 elements in width
+//     sum += array[end];
+    
+//     // Once we have the sum of three elements
+//     if (end >= k - 1){
+
+//       // MY SOLUTION
+//       // If maxSum is undefined
+//       if (maxSum === undefined) {
+//         maxSum = sum;
+//       }
+
+//       // Check if sum is greater than current max
+//       if (sum > maxSum) {
+//         maxSum = sum;
+//       }
+      
+//       //REFACTORED VERSION (but this version doesn't account for the possibility that the maxSum could be less than 0 if we initialize maxSum (line 34) at 0)
+//       // maxSum = Math.max(sum, maxSum);
+
+//       // Subtract first element from sum
+//       sum -= array[start];
+      
+//       // Increment start (sliding window at beginning)
+//       start += 1;
+//     }
+//   }
+//   return maxSum;
+// }
+
+// console.log(MaxSum([2, 1, 5, 1, 3, 2], 3)) // Output: 9
+
+
+// Smallest Subarray with a Greater Sum  (given they are all positive numbers)
+  // Time Complexity: 0(n)
+  // Space Complexity: 0(1)
+
+const smallestLength = (array, s) => {
+  let L = Infinity,
+    start = 0,
+    sum = 0
+  
+  // If 's' already exists in the array, no need to go in the loop
+  if (array.includes(s)) return 1;
+
+  for (let end = 0; end < array.length; end++) {
+    sum += array[end] // add each element to sum
+
+    while (sum >= s){
+      L = Math.min(L, end - start + 1); // compare current l to  current window (but shouldn't this only check if it's equal??)
+      sum -= array[start]; // subtract start element from sum
+      start += 1; // move start window to right by one element
+    }
+  }
+  if (L === Infinity) return 0; // if nothing added to s
+
+  return L; 
+}
+
+console.log(smallestLength([2, 1, 5, 2, 3, 2], 7)) // Output: 2
+console.log(smallestLength([2, 7, 5, 2, 3, 2], 7)) // Output: 1
+
