97c4d841f1201509e9acb1116b0e8896c9e740fc
Erika Collins <erikasusancollins@gmail.comclear>
2022-09-21 21:05:28 -0600

    wednesday

--- TestingNotes/testingUnit.js
+++ TestingNotes/testingUnit.js
@@ -61,3 +61,16 @@ expect(result).toEqual(4);
       -- Write the code to implement your feature by following the errors, and pass the test
       -- Repeat
 */
+
+/*
+NOTES:
+  - in Jest we use the "describe" function to separate our tests into sections. They make our test outputs readable
+  - You can place "beforeAll", "beforeEach", "afterAll", and "afterEach"
+  functions inside of "describe" blocks and they will only run for tests
+  inside that describe block. You can even nest describes within describes!
+  - Jest runs the "beforeAll" function once, before any tests are executed.
+  Here, we write to the file and then reset our database model. Then, we
+  invoke the "done" callback to tell Jest our async operations have
+  completed. This way, the tests won't start until the "database" has been
+  reset to an empty Array!
+*/
